a73eb9c47e61e4f3db967ca7045dc86fbaf9a65c
pikjung <tanjungfikri1@gmail.com>
2021-05-27 10:04:38 +0700

    Contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fikri\Belajar;
+
+/**
+ *
+ */
+class Customer
+{
+
+  function __construct($name)
+  {
+
+  }
+
+  public function sayHello($name)
+  {
+    return "Hello $name, My name is $this->name";
+  }
+}
+
+
+ ?>
